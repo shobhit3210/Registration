e988b5336c09cb7f2b404e2ee5be45da44f33ae9
Shobhit <shobhit210@outlook.com>
2018-07-09 13:57:38 +0530

    Start1.3

---
+++ src/main/java/org/registration/model/User.java
@@ -0,0 +1,79 @@
+package org.registration.model;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private String fullName;
+    private String gender;
+    private String address;
+    private String country;
+    private ArrayList<String> interests=new ArrayList<String>();
+    private String picAddress;
+    private String email;
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public ArrayList<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(ArrayList<String> interests) {
+        this.interests = interests;
+    }
+
+    public String getPicAddress() {
+        return picAddress;
+    }
+
+    public void setPicAddress(String picAddress) {
+        this.picAddress = picAddress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
